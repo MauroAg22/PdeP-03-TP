c08307d889c2cf5b1b7b4fec7f3a1560eb324e67
Mauro Agustín Lucero <mauroaglucero@gmail.com>
2025-10-01 06:29:41 -0300

    index on main: 937174b Se agregaron funciones y validaciones para las fechas al crear un objeto de tipo Tarea

---
+++ dist/lib/constantes.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ESTADO = exports.PRIORIDAD = void 0;
+const PRIORIDAD = [
+    "+--",
+    "++-",
+    "+++"
+];
+exports.PRIORIDAD = PRIORIDAD;
+const ESTADO = [
+    "pendiente",
+    "en progreso",
+    "completada",
+    "cancelada"
+];
+exports.ESTADO = ESTADO;
