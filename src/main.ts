d27e6ef8ec64f2a26fedb064e4574322f578f358
Mauro Agustín Lucero <mauroaglucero@gmail.com>
2025-10-06 17:51:59 -0300

    Se finalizó la opción de ver tareas donde filtra y da opción a editar

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { fechaToString } from "./lib/funciones";
 import { input, close } from "./lib/input";
 
 
-let tareas: Tarea[] = [];
-
+// Menús de opciones
 function menuPrincipal(): void {
     console.log('-------- Menú de Opciones --------\n');
     console.log('[1] Mostrar tareas');
@@ -73,8 +72,8 @@ async function main(): Promise<void> {
     let tarea4 = new Tarea(
         "Leer un libro",
         "Terminar de leer 'El Principito'",
-        PRIORIDAD[3],
-        ESTADO[1],
+        PRIORIDAD[2],
+        ESTADO[3],
         new Date(fechaToString("2026", "01", "05") + "T03:00:00Z") // UTC-3
     );
 
@@ -87,8 +86,6 @@ async function main(): Promise<void> {
     miToDoList.agregarTarea(tarea2);
     miToDoList.agregarTarea(tarea3);
     miToDoList.agregarTarea(tarea4);
-
-    console.clear();
 
     let opcionMenu: number;
 
@@ -104,66 +101,197 @@ async function main(): Promise<void> {
                 console.clear();
                 let opcionTareasAVer: number;
                 let arregloTareasFiltradas: Tarea[] = [];
+                let idTareaAVer: number;
+                let idVerMasDetalles: number[] = [];
+                let sePuedeVerLaTarea: boolean;
+                let opcionQuiereEditar: number;
+                let quiereEditar: boolean;
+                let opcionCampoAEditar: number;
+                let cancelarEdicion: boolean = false;
 
-                let variableDePrueba: number = 0;
+                // Variables para edición de tarea
+                let nuevoTitulo: string;
+                let nuevaDescripcion: string;
+                let nuevaPrioridad: number;
+                let nuevoEstado: number;
+                let nuevoAnio: string;
+                let nuevoMes: string;
+                let nuevoDia: string;
+
+                if (miToDoList.getTareas().length === 0) {
+                    console.log("No hay tareas cargadas.\n");
+                    await input('Presione Enter para continuar...');
+                    console.clear();
+                    break;
+                }
 
                 do {
                     tareasAVer();
-                    opcionMenu = parseInt(await input('> '), 10);
-                    opcionTareasAVer = 1; // Aquí se debería obtener la opción del usuario, por ahora está fijo.
-
-                    if (opcionTareasAVer >= 1 && opcionTareasAVer <= 5) {
+                    opcionTareasAVer = parseInt(await input('> '), 10);
+                    if (opcionTareasAVer >= 0 && opcionTareasAVer <= 5) {
                         console.clear();
-                        arregloTareasFiltradas = miToDoList.arrayFiltrarPorEstado(opcionTareasAVer);
-
-                        let idTareaAVer: number;
-                        let idDisponibleParaBorrar: number[] = [];
-
+                        if (opcionTareasAVer !== 0) {
+                            arregloTareasFiltradas = miToDoList.arrayFiltrarPorEstado(opcionTareasAVer - 2);
+                        }
                         do {
-                            console.log('---------- Tus tareas pendientes son las siguientes ----------------\n');
-                            for (let tarea of arregloTareasFiltradas) {
-                                console.log(`[${tarea.getId()}] - ${tarea.getTitulo()}`);
-                                idDisponibleParaBorrar[idDisponibleParaBorrar.length] = tarea.getId();
+                            if (opcionTareasAVer - 1 === 0) {
+                                console.log('--------------- Tus tareas son las siguientes ----------------------\n');
+                                for (let tarea of miToDoList.getTareas()) {
+                                    console.log(`[${tarea.getId()}] - ${tarea.getTitulo()}`);
+                                    idVerMasDetalles[idVerMasDetalles.length] = tarea.getId();
+                                }
+                                console.log('\n--------------------------------------------------------------------\n');
+                            } else {
+
+                                if (arregloTareasFiltradas.length === 0) {
+                                    console.log("No hay tareas con ese estado.\n");
+                                    await input('Presione Enter para continuar...');
+                                    console.clear();
+                                    break;
+                                }
+
+                                console.log('---------- Tus tareas pendientes son las siguientes ----------------\n');
+                                for (let tarea of arregloTareasFiltradas) {
+                                    console.log(`[${tarea.getId()}] - ${tarea.getTitulo()}`);
+                                    idVerMasDetalles[idVerMasDetalles.length] = tarea.getId();
+                                }
+                                console.log('\n--------------------------------------------------------------------\n');
                             }
-                            console.log('\n--------------------------------------------------------------------\n');
-
-                            console.log(idDisponibleParaBorrar);
-
-                            console.log('Ingrese el ID de la tarea que desea ver en detalle.\n\n> ');
-                            idTareaAVer = 1; // Aquí se debería obtener el ID del usuario, por ahora está fijo
-
-                            let sePuedeVerLaTarea: boolean = false;
-
-                            for (let id of idDisponibleParaBorrar) {
+                            console.log('Ingrese el ID de la tarea que desea ver en detalle.\n\n');
+                            idTareaAVer = parseInt(await input('> '), 10);
+                            sePuedeVerLaTarea = false;
+                            for (let id of idVerMasDetalles) {
                                 if (id === idTareaAVer) {
                                     sePuedeVerLaTarea = true;
                                 }
                             }
-
                             if (sePuedeVerLaTarea) {
                                 console.clear();
                                 miToDoList.mostrarDetallesDeTarea(idTareaAVer);
+
+                                console.log("¿Desea editar esta tarea?\n");
+                                console.log("[1] Sí");
+                                console.log("[2] No\n");
+
+                                opcionQuiereEditar = parseInt(await input('> '), 10);
+                                quiereEditar = false;
+
+                                if (opcionQuiereEditar === 1) {
+                                    quiereEditar = true;
+                                }
+
+                                if (quiereEditar) {
+
+                                    do {
+
+                                        console.clear();
+                                        console.log(`Estás editando la tarea: ${miToDoList.getUnaTarea(idTareaAVer).getTitulo()}\n`);
+                                        console.log(`[1] Título: ${miToDoList.getUnaTarea(idTareaAVer).getTitulo()}`);
+                                        console.log(`[2] Descripción: ${miToDoList.getUnaTarea(idTareaAVer).getDescripcion()}`);
+                                        console.log(`[3] Prioridad: ${miToDoList.getUnaTarea(idTareaAVer).getPrioridad()}`);
+                                        console.log(`[4] Estado: ${miToDoList.getUnaTarea(idTareaAVer).getEstado()}`);
+                                        console.log(`[5] Fecha de vencimiento: ${miToDoList.getUnaTarea(idTareaAVer).getFechaVencimiento().toLocaleDateString()}\n`);
+
+                                        console.log("[0] Volver al menú principal\n");
+
+                                        console.log("Ingrese el número del campo que desea editar.\n");
+                                        opcionCampoAEditar = parseInt(await input('> '), 10);
+
+                                        switch (opcionCampoAEditar) {
+                                            case 1:
+                                                // Editar título
+                                                nuevoTitulo = await input('Ingrese el nuevo título: ');
+                                                miToDoList.getUnaTarea(idTareaAVer).setTitulo(nuevoTitulo);
+                                                console.log("\nTítulo editado con éxito.\n");
+                                                await input('Presione Enter para continuar...');
+                                                break;
+                                            case 2:
+                                                // Editar descripción
+                                                nuevaDescripcion = await input('Ingrese la nueva descripción: ');
+                                                miToDoList.getUnaTarea(idTareaAVer).setDescripcion(nuevaDescripcion);
+                                                console.log("\nDescripción editada con éxito.\n");
+                                                await input('Presione Enter para continuar...');
+                                                break;
+                                            case 3:
+                                                // Editar prioridad
+                                                do {
+                                                    console.clear();
+                                                    console.log("\nSeleccione la nueva prioridad:\n");
+                                                    console.log(`[1] - ${PRIORIDAD[0]}`);
+                                                    console.log(`[2] - ${PRIORIDAD[1]}`);
+                                                    console.log(`[3] - ${PRIORIDAD[2]}\n`);
+                                                    nuevaPrioridad = parseInt(await input('> '), 10);
+
+                                                    if (nuevaPrioridad >= 1 && nuevaPrioridad <= 3) {
+                                                        miToDoList.getUnaTarea(idTareaAVer).setPrioridad(PRIORIDAD[nuevaPrioridad - 1]);
+                                                        console.log("\nPrioridad editada con éxito.\n");
+                                                        await input('Presione Enter para continuar...');
+                                                    } else {
+                                                        console.log("\nPrioridad no válida. Por favor, seleccione una prioridad válida.\n");
+                                                        await input('Presione Enter para continuar...');
+                                                    }
+                                                } while (nuevaPrioridad < 1 || nuevaPrioridad > 3);
+                                                break;
+                                            case 4:
+                                                // Editar estado
+                                                do {
+                                                    console.clear();
+                                                    console.log("\nSeleccione el nuevo estado:\n");
+                                                    console.log(`[1] - ${ESTADO[0]}`);
+                                                    console.log(`[2] - ${ESTADO[1]}`);
+                                                    console.log(`[3] - ${ESTADO[2]}`);
+                                                    console.log(`[4] - ${ESTADO[3]}\n`);
+                                                    nuevoEstado = parseInt(await input('> '), 10);
+                                                    if (nuevoEstado >= 1 && nuevoEstado <= 4) {
+                                                        miToDoList.getUnaTarea(idTareaAVer).setEstado(ESTADO[nuevoEstado - 1]);
+                                                        console.log("\nEstado editado con éxito.\n");
+                                                        await input('Presione Enter para continuar...');
+                                                    } else {
+                                                        console.log("\nEstado no válido. Por favor, seleccione un estado válido.\n");
+                                                        await input('Presione Enter para continuar...');
+                                                    }
+                                                } while (nuevoEstado < 1 || nuevoEstado > 4);
+                                                break;
+                                            case 5:
+                                                // Editar fecha de vencimiento
+                                                console.clear();
+                                                await input('Esta opción aún no está disponible. Presione Enter para continuar...');
+                                                break;
+                                            case 0:
+                                                // Volver al menú principal
+                                                quiereEditar = false;
+                                                cancelarEdicion = true;
+                                                break;
+                                            default:
+                                                // Opción no válida
+                                                console.log("\nOpción no válida\n");
+                                                await input('Presione Enter para continuar...');
+                                                break;
+                                        }
+
+                                        // await input('Pausa de prueba.');
+                                    } while (opcionCampoAEditar < 0 || opcionCampoAEditar > 5 || !cancelarEdicion);
+
+                                } else {
+                                    console.log("No se editará la tarea.");
+                                }
+
                             } else {
+                                console.log("\nID de tarea no válido. Intente nuevamente.\n");
+                                await input('Presione Enter para continuar...');
                                 console.clear();
-                                console.log("ID de tarea no válido. Intente nuevamente.");
                             }
-
-
-                        } while (false);
-
-
+                        } while (!sePuedeVerLaTarea);
 
                     } else if (opcionTareasAVer === 0) {
                         console.clear();
                         break;
+                    } else {
+                        console.log("\nOpción no válida. Intente nuevamente.\n");
+                        await input('Presione Enter para continuar...');
                     }
-
-                    variableDePrueba++;
-                } while (opcionTareasAVer < 1 || opcionTareasAVer > 5 || variableDePrueba === 5);
-
-
-                salirDelMenu = true;
-                // console.clear();
+                } while (opcionTareasAVer < 0 || opcionTareasAVer > 5);
+                console.clear();
                 break;
             case 2:
                 // Agregar tarea
